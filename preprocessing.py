6dcccf5fd6116e18ed605bc6d9b2a60cc5d23c83
mariomalak1 <marioalabd611@gmail.com>
2025-05-05 02:38:53 +0300

    preprocessing for problem

---
+++ preprocessing.py
@@ -0,0 +1,55 @@
+from LoadData import LoadData 
+from Partitioning import Partitioning
+from Normalization import Normalization
+from knn import KNN
+from NeuralNetwork import NeuralNetwork
+from Evaluation import Evaluation
+
+
+def preprocessing(fileName, percentageOfRowsToRead=70, train_set_size=75, knn_k=3, learning_rate=0.005, hidden_layers=2):
+    loadData = LoadData(fileName, percentageOfRowsToRead)
+    df = loadData.loadData()
+
+    # remove "id" column
+    df = df.drop('id', axis=1)
+
+    # remove all nulls 
+    df = df.dropna()
+
+    # make data without class label as notckd
+    # df["classification"] = df["classification"].fillna("notckd")
+
+    normalization = Normalization(df)
+    normalization.convertAllCategoricalFeaturesToNumeric()
+    normalization.normalize()
+    
+    partitioning = Partitioning(normalization.df)
+
+
+    x_train, y_train, x_test, y_test = partitioning.split(train_size=train_set_size)
+    
+    ## KNN
+    knn = KNN(k=knn_k)
+    knn.fit(x_train.to_numpy(), y_train.to_numpy())
+    
+    # Make predictions
+    predictions = knn.predict(x_test.to_numpy())
+
+    # Test with known labels (for accuracy calculation)
+    knn_accuracy = Evaluation.accuracy(predictions, y_test)
+    print(f"KNN - Accuracy: {knn_accuracy * 100}%")
+
+
+    ## ANN
+    nn = NeuralNetwork(input_size=x_train.shape[1], hidden_size=hidden_layers, output_size=1, learning_rate=learning_rate)
+    nn.train(x_train.to_numpy(), y_train.to_numpy(), epochs=1000)
+
+    predictions = nn.predict(x_test.to_numpy())
+
+    # Test with known labels (for accuracy calculation)
+    ann_accuracy = Evaluation.accuracy(predictions, y_test)
+    print(f"ANN - Accuracy: {ann_accuracy * 100}%")
+
+    return knn_accuracy, ann_accuracy 
+
+
