3910433d96b1fa8ed28dbbb939c7bc13d1262b29
mariomalak1 <marioalabd611@gmail.com>
2025-05-14 02:44:20 +0300

    remove comments

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class NeuralNetwork:
     def __init__(self, input_size, hidden_size, output_size, learning_rate=0.1):
-        # Weight initialization
+        # weights initialization
         self.learning_rate = learning_rate
         self.weights_input_hidden = np.random.randn(input_size, hidden_size)
         self.bias_hidden = np.zeros((1, hidden_size))
@@ -16,7 +16,6 @@ class NeuralNetwork:
         return x * (1 - x)
 
     def forward(self, X):
-        # Forward propagation
         self.z1 = np.dot(X, self.weights_input_hidden) + self.bias_hidden
         self.a1 = self.sigmoid(self.z1)
         self.z2 = np.dot(self.a1, self.weights_hidden_output) + self.bias_output
@@ -27,16 +26,16 @@ class NeuralNetwork:
         return np.mean((y_true - y_pred) ** 2)
 
     def backward(self, X, y_true, y_pred):
-        # Output layer error
         y_true = y_true.reshape(-1, 1)
+        # output layer error calc
         output_error = y_true - y_pred
         output_delta = output_error * self.sigmoid_derivative(y_pred)
 
-        # Hidden layer error
+        # hidden layer error calc
         hidden_error = np.dot(output_delta, self.weights_hidden_output.T)
         hidden_delta = hidden_error * self.sigmoid_derivative(self.a1)
 
-        # Weight and bias updates
+        # weight and bias updates
         self.weights_hidden_output += self.learning_rate * np.dot(self.a1.T, output_delta)
         self.bias_output += self.learning_rate * np.sum(output_delta, axis=0, keepdims=True)
         self.weights_input_hidden += self.learning_rate * np.dot(X.T, hidden_delta)
